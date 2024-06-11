02cf5e3b8707f36233566b86e4cfd352bcdaeec7
Andrew <jichara.908@gmail.com>
2024-06-11 19:34:53 +0300

    we put the code in order and print the result

--- Program.cs
+++ Program.cs
@@ -1,15 +1,31 @@
-string[] array1 = new string[6] {"257", "45", "hello", "world", "res","456"};
-string[] array2 = new string[array1.Length];
-
-void SecondArray(string[] array1, string[] array2)
+internal class Program
 {
-    int count = 0;
-    for (int i = 0; i < array1.Length; i++)
+    private static void Main(string[] args)
     {
-    if(array1[i].Length <= 3)
+        string[] array1 = new string[6] { "257", "45", "hello", "world", "res", "456" };
+        string[] array2 = new string[array1.Length];
+
+        void SecondArray(string[] array1, string[] array2)
         {
-        array2[count] = array1[i];
-        count++;
+            int count = 0;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if (array1[i].Length <= 3)
+                {
+                    array2[count] = array1[i];
+                    count++;
+                }
+            }
         }
+        void PrintArray(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+        }
+        SecondArray(array1, array2);
+        PrintArray(array2);
     }
-} 
+}
